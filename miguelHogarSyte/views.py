2c1e557623fb4265283388321e8c8e6abfd50088
Carlos <carlos77alvarez@gmail.com>
2016-10-03 08:52:45 -0300

    uri chanta

--- miguelHogarSyte/views.py
+++ miguelHogarSyte/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Producto
 def productos(request):
-    productos= Producto.objects.filter(rubro__nombre='Electrodomesticos')
+    productos=Producto.objects.all()
+    #productos= Producto.objects.filter(rubro__nombre='Electrodomesticos')
     return render(request, 'miguelHogarSyte/rubro.html', {'productos': productos})
 
 # Create your views here.
